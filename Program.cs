36e3082d8109f2091ddf2b4963b18661d48fee52
Tik88 <xtikx@yandex.ru>
2021-10-27 15:56:24 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string q = Console.ReadLine();
+
+            string[] w = q.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+            int max = w.Max(x => x.Length);
+            Console.WriteLine("Самое длинное слово из {0} букв", max);
+            Console.ReadLine();
+
+        }
+    }
+}
